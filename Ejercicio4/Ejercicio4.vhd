2905079969e2fb0eccb65db27b7f8494e6d7dc61
Ian <yoian@outlook.com>
2021-10-28 20:54:33 -0500

    CreaciÃon del proyecto para el ejercicio 4 de la practica 4. Tambien se añadio el archivo vhd asi como el ucf para las conexiones de la FPGS

---
+++ Ejercicio4/Ejercicio4.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:37:05 10/28/2021 
+-- Design Name: 
+-- Module Name:    Ejercicio4 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Ejercicio4 is
+end Ejercicio4;
+
+architecture Behavioral of Ejercicio4 is
+
+begin
+
+
+end Behavioral;
+
