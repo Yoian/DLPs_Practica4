742dbab7dea58597921c7182ec835d573689613e
Ian <yoian@outlook.com>
2021-10-29 23:00:54 -0500

    Cambio del debouce

--- Ejercicio2a/Ejercicio2a.vhd
+++ Ejercicio2a/Ejercicio2a.vhd
@@ -6,7 +6,8 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Ejercicio2a is
-	Generic(N:integer:=5);
+	Generic(N:integer:=5;
+				COUNT_MAX : integer := 5);
 	port(
 		clk,boton: in std_logic;
 		leds: out std_logic;
@@ -24,8 +25,10 @@ architecture Behavioral of Ejercicio2a is
 	signal senal: std_logic:='0';
 	signal estado:  integer range 0 to 1:= 0;
 	
---	signal cnt: integer range 0 to 31:= 0; 
---	signal Signalservo: std_logic; 
+	signal count : integer := 0;
+	type state_type is (idle,wait_time); --state machine
+	signal state : state_type := idle;
+
 	
 begin
 	divisor:process(clk)
@@ -35,29 +38,57 @@ begin
 		end if;
 	end process divisor;
 	
-	debounce:process(clk,delay1,delay2,delay3)
+	debounce: process(clkdiv(N))
 	begin
-		if rising_edge(clk) then
-			delay1<=boton;
-			delay2<=delay1;
-			delay3<=delay2;
-		end if;
-		senal<=delay1 and delay2 and delay3;
+		if rising_edge(clkdiv(N)) then
+			case (state) is
+				when idle =>
+					if boton = '1' then  
+					  state <= wait_time;
+					else
+					  state <= idle; --wait until button is pressed.
+					end if;
+					senal <= '0';
+				when wait_time =>
+					if(count = COUNT_MAX) then
+						count <= 0;
+						if boton = '1' then
+							senal <= '1';
+						end if;
+						 state <= idle;  
+					else
+						count <= count + 1;
+					end if; 
+			end case;       
+		 end if;        
 	end process debounce;
 	leds<=senal;
 	
-	activacion: process(senal)
+--	debounce:process(clk,delay1,delay2,delay3)
+--	begin
+--		if rising_edge(clk) then
+--			delay1<=boton;
+--			delay2<=delay1;
+--			delay3<=delay2;
+--		end if;
+--		senal<=delay1 and delay2 and delay3;
+--	end process debounce;
+--	leds<=senal;
+	
+	activacion: process(clkdiv(N),senal)
 	begin
-		if rising_edge(senal) then
-			estado<=estado+1;
+		if rising_edge(clkdiv(N)) then
+			if senal='1' then
+				estado<=estado+1;
+			end if;
 		end if;
 	end process activacion;
 	
-	MaqEdo:process(clkdiv(N),estado)
+	MaqEdo:process(estado,clkdiv(N))
 	begin
 		if rising_edge(clkdiv(N)) then
 			case FSM is
-				when EP => --Primer estado cuando AB = "00"
+				when EP => 
 					if estado = 0 then 
 						FSM <= EP; 
 						motores<="00";
@@ -65,7 +96,7 @@ begin
 						FSM <= EA; 
 						motores<="11"; 
 					end if;
-				when EA => --Segundo estado cuando AB = "10"
+				when EA =>
 					if estado = 0 then 
 						FSM <= EP; 
 						motores<="00";
