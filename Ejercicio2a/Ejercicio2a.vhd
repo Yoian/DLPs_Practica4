10e8a749ee80e28c27020f17376d1f65a4cd35fc
Ian <yoian@outlook.com>
2021-10-29 20:30:39 -0500

    Implementacion de un codigo el cual aun presenta problemas de latch

--- Ejercicio2a/Ejercicio2a.vhd
+++ Ejercicio2a/Ejercicio2a.vhd
@@ -2,19 +2,55 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Ejercicio2a is
+	Generic(N:integer:=5);
 	port(
-		boton: in std_logic;
-		leds: out std_logic_vector(0 to 2);
+		clk,boton: in std_logic;
+		leds: out std_logic;
 		motores: out std_logic_vector(1 downto 0));
 end Ejercicio2a;
 
 architecture Behavioral of Ejercicio2a is
-	signal delay1,delay2,delay3,senal: std_logic;
+	type edos is (EA,EP);
+	signal ASM:edos:=EA;
+	
+	signal delay1,delay2,delay3,delay4,senal: std_logic;
+	signal clkdiv: std_logic_vector(N downto 0);
+	signal estado:  integer range 0 to 1:= 0;
 begin
 
+	divisor:process(clk)
+	begin
+		if rising_edge(clk) then
+			clkdiv<=clkdiv + 1;
+		end if;
+	end process divisor;
+	
+	debounce:process(clkdiv(N),delay1,delay2,delay3)
+	begin
+		if rising_edge(clkdiv(N)) then
+			delay1<=boton;
+			delay2<=delay1;
+			delay3<=delay2;
+		end if;
+		senal<=delay1 and delay2 and delay3;
+	end process debounce;
+	leds<=senal;
+	
+	activacion: process(boton,estado,clk)
+	begin
+		if rising_edge(clk)and boton='1' then
+			estado<=estado+1;
+		end if;
+		if estado=1 then
+			motores<="11";
+		else 
+			motores<="00";
+		end if;
+	end process activacion;
 
 end Behavioral;
 
